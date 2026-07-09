d07c0b7178d49790dd2a76acab52a79b69b711a5
Kevin Jose <mjkevin2006@gmail.com>
2026-07-09 22:55:32 +0530

    Question 1

--- Simple linear regression.py
+++ Simple linear regression.py
@@ -1,3 +1,14 @@
+'''
+Implement linear regression with one variable on the California Housing dataset to predict
+housing prices based on a single feature (e.g., the average number of rooms per dwelling).
+Tasks:
+● Load and preprocess the datase.
+● Implement linear regression using both gradient descent and the normal equation.
+● Evaluate the model performance using metrics such as Mean Squared Error
+(MSE) and R-squared.
+● Visualize the fitted line along with the data points.
+'''
+
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import fetch_california_housing
