b79a81c2cabdc391dbf27a997e5130eb50e34ee6
Kevin Jose <mjkevin2006@gmail.com>
2026-07-30 00:05:40 +0530

    Question 6

--- Logistic regression.py
+++ Logistic regression.py
@@ -1,3 +1,13 @@
+'''
+Implement a logistic regression model to predict the likelihood of a disease using the Pima
+Indians Diabetes dataset. Compare the performance with and without feature scaling.
+Tasks:
+● Load and preprocess the Pima Indians Diabetes dataset.
+● Implement logistic regression for binary classification.
+● Evaluate model performance with and without feature scaling.
+● Analyze metrics such as accuracy, precision, recall, and F1-score.
+'''
+
 import numpy as np
 from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
