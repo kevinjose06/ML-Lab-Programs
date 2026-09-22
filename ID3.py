dc3ccbe10a1ac7d7a3750f15174d628e39b140a9
Kevin Jose <mjkevin2006@gmail.com>
2026-09-22 22:28:15 +0530

    Question 9

--- ID3.py
+++ ID3.py
@@ -19,21 +19,26 @@ from sklearn.tree import DecisionTreeClassifier,plot_tree
 online_retail=fetch_ucirepo(id=352)
 df=online_retail.data.original
 print("original shape: ",df.shape)
+
 df=df.dropna(subset=["CustomerID"])
 df=df[~df["InvoiceNo"].astype(str).str.startswith("C")]
 df=df[(df["Quantity"]>0)&(df["UnitPrice"]>0)]
 df["TotalAmount"]=df["Quantity"]*df["UnitPrice"]
 print("After preprocessing: ",df.shape)
+
 customer_data=df.groupby("CustomerID").agg(TotalSpent=("TotalAmount","sum"),TotalQuantity=("Quantity","sum"),NumInvoices=("InvoiceNo","nunique"),UniqueProducts=("StockCode","nunique"),AvgOrderValue=("TotalAmount","mean")).reset_index()
 customer_data["PurchaseFrequency"]=(customer_data["NumInvoices"]/customer_data["NumInvoices"].max())
 customer_data["Segment"]=pd.qcut(customer_data["TotalSpent"],q=3,labels=["Low","Medium","High"])
+
 print("Segment Contribution: ")
 print(customer_data["Segment"].value_counts())
 features=["TotalQuantity","NumInvoices","UniqueProducts","AvgOrderValue","PurchaseFrequency"]
 
 X=customer_data[features]
 y=customer_data["Segment"]
+
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42,stratify=y)
+
 id3_model=DecisionTreeClassifier(criterion="entropy",max_depth=4,random_state=42)
 id3_model.fit(X_train,y_train)
 y_pred=id3_model.predict(X_test)
